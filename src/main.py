2559c64147283ab8ce589369d5365c646e1f54ed
Manjunath Somashekar <ujnamss@gmail.com>
2019-04-19 11:52:21 -0700

    updated code to use zb_restqa library

--- src/main.py
+++ src/main.py
@@ -1,4 +1,10 @@
-from restqa import RestTestsuiteDriver
+from __future__ import print_function
+from __future__ import unicode_literals
+from __future__ import division
+from __future__ import absolute_import
+from future import standard_library
+standard_library.install_aliases()
+from zb_restqa import RestTestsuiteDriver
 
 def sample_setup(ctxt):
     print("you could do any setup that is needed for the suite")
@@ -9,8 +15,21 @@ def sample_teardown(ctxt):
     ctxt.clear()
     print("you can release any resource")
 
+def pre_test_1(ctxt):
+    print("you could do any setup that is needed for the suite")
+    ctxt["pre_test_1"] = "123"
+    return ctxt
+
+def post_test_1(ctxt):
+    print("you could do any setup that is needed for the suite")
+    ctxt["post_test_1"] = "123"
+    return ctxt
+
 driver = RestTestsuiteDriver("../tests", {
     'sample_setup': sample_setup,
-    'sample_teardown': sample_teardown
+    'sample_teardown': sample_teardown,
+    'pre_test': pre_test_1,
+    'post_test': post_test_1
 })
+
 driver.run_tests()
